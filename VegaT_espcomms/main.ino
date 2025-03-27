713c4b76ef707d578b74c6ea20beb38b865a4dac
Varu2404 <varun.nair2404@gmail.com>
2025-03-27 17:46:22 +0530

    Update main.ino

--- VegaT_espcomms/main.ino
+++ VegaT_espcomms/main.ino
@@ -1,9 +1,12 @@
-//#include "BluetoothSerial.h"
-//#include <SoftwareSerial.h>
+#include "BluetoothSerial.h"
+#include <SoftwareSerial.h>
 //#include <string.h>
 #include <stdio.h>
-
+#include "bluetoothcomms.h"
+#include "VegaT_espcomms.h"
 bool connectionstatus = false;
+SoftwareSerial uart = SoftwareSerial(16,17);
+BluetoothSerial SerialBT;
 
 void setup(){  
   uart.begin(9600);
@@ -12,17 +15,22 @@ void setup(){
   Bluetoothinit();
 }
 
-void loop(){
-  //bool connectionstatus;
-   if(connectionstatus= false ){
-   if(SerialBT.connected()){
-    Serial.println("Connected to mobile device");
-    connectionstatus= true ;
+void loop() {
+  // Check if the client is connected
+  if (!SerialBT.hasClient() && isAuthenticated) {
+    Serial.println("Client disconnected. Resetting authentication.");
+    isAuthenticated = false; // Reset authentication state
+    hasPrompted = false;     // Reset the prompt flag
   }
-   }
-  if (SerialBT.available()) { 
-    bluetoothcomm();
+
+  // Prompt for password if a new client connects and not authenticated
+  if (SerialBT.hasClient() && !isAuthenticated && !hasPrompted) {
+    SerialBT.println("Enter password:");
+    hasPrompted = true;
   }
-  
+bluetoothAuthentication();
+if (isauth()){
+ bluetoothcomm();
+}
   delay(20);
 }
